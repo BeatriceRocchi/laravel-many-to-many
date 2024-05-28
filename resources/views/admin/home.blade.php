4683ddd6dc2a7fc0499e6f8a79ec5165cd02cc56
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-05-28 18:13:12 +0200

    [add] links in dashboard home to create view for projects and [add] links in dashboard home to index views for types and technologies

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,19 +2,25 @@
 
 @section('content')
     <div class="wrapper-dashboard d-flex justify-content-around align-items-center my-4 flex-wrap">
+
         <div class="highlights-box d-flex align-items-center justify-content-center">
             <i class="fa-solid fa-laptop-code"></i>
             <div><span class="fw-semibold">{{ $projects_tot }}</span> projects</div>
+            <a href="{{ route('admin.projects.create') }}"><i class="fa-solid fa-circle-plus icon-custom-add"></i></a>
         </div>
+
+
 
         <div class="highlights-box d-flex align-items-center justify-content-center">
             <i class="fa-solid fa-layer-group"></i>
             <div><span class="fw-semibold">{{ $types_tot }}</span> types</div>
+            <a href="{{ route('admin.types.index') }}"><i class="fa-solid fa-circle-plus icon-custom-add"></i></a>
         </div>
 
         <div class="highlights-box d-flex align-items-center justify-content-center">
             <i class="fa-solid fa-robot"></i>
             <div><span class="fw-semibold">{{ $technologies_tot }}</span> technologies</div>
+            <a href="{{ route('admin.technologies.index') }}"><i class="fa-solid fa-circle-plus icon-custom-add"></i></a>
         </div>
     </div>
 @endsection
