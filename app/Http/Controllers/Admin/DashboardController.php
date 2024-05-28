72a5b336e935a64d42c055410354a64fd3b4d448
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-05-28 18:02:19 +0200

    [add] counter of projects, types and technologies on dashboard home

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
+use App\Models\Type;
+use App\Models\Technology;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.home');
+        $projects_tot = Project::count();
+        $types_tot = Type::count();
+        $technologies_tot = Technology::count();
+        return view('admin.home', compact('projects_tot', 'types_tot', 'technologies_tot'));
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1 class="text-center">Home admin</h1>
+    <div class="wrapper-dashboard d-flex justify-content-around align-items-center my-4 flex-wrap">
+        <div class="highlights-box d-flex align-items-center justify-content-center">
+            <i class="fa-solid fa-laptop-code"></i>
+            <div><span class="fw-semibold">{{ $projects_tot }}</span> projects</div>
+        </div>
+
+        <div class="highlights-box d-flex align-items-center justify-content-center">
+            <i class="fa-solid fa-layer-group"></i>
+            <div><span class="fw-semibold">{{ $types_tot }}</span> types</div>
+        </div>
+
+        <div class="highlights-box d-flex align-items-center justify-content-center">
+            <i class="fa-solid fa-robot"></i>
+            <div><span class="fw-semibold">{{ $technologies_tot }}</span> technologies</div>
+        </div>
+    </div>
 @endsection
